9f2315b8e9c7e6cd338ef8a72afdd3bdf6b1e579
yongjae5717 <dydwo5717@naver.com>
2023-02-08 23:01:23 +0900

    layer3: fat32.py 불필요한 코드 삭제

--- file_system/layer_3/fat32.py
+++ file_system/layer_3/fat32.py
@@ -5,9 +5,6 @@ from file_system.layer_2.node import *
 class Fat32:
     def __init__(self, filename):
         self.filename = filename
-        self.bootRecord = BootRecord(filename)
-        self.fatTable = FatTable(self.bootRecord)
-        self.dir_pre = DirectoryEntry(self.bootRecord)
 
     def BuildFileSystem(self):
         node_stream = NodeStream(self.filename)
